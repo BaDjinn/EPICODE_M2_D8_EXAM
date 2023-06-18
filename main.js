f3328d684c92584bc6b58e1eb25237b57b96dbb7
badjinn <g.sottovia@yahoo.com>
2023-06-18 20:19:42 +0200

    Good to go!

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {jobs} from './scaricabile.js';
+const listLocation = document.getElementById("jobResultList")
 
 // function search (searchTitle, searchLocation, arr){ 
 //     //console.log(jobs.length);
@@ -35,7 +36,6 @@ import {jobs} from './scaricabile.js';
 //     console.log(search2("marketing","us",jobs));
   
   
-  
 let compareStrings = (strCercata,testoDaFiltrare) => {
       const arrStrCercata = strCercata.toLowerCase().split(" ");
       //console.log(arrStrCercata);
@@ -60,10 +60,38 @@ function search3(searchTitle, searchLocation,arr){
   
   
     //console.log("prova di comparazione: ",compareStrings("for no the","Aint No Rest For tHe Wicked"));
-    // console.log(search3("marketing","us",jobs));
+  //console.log(search3("marketing","us",jobs));
 
-function printResults() {
+const buttonListen = document.getElementById("searchButton");
+ 
+const printResults = () => {  
   const inJob = document.getElementById("inJob").value;
-  console.log(inJob);
-  
+  const inPlace = document.getElementById("inPlace").value;
+  const jobResults = search3(inJob,inPlace,jobs);
+  console.log(jobResults);
+  if (jobResults.count!=0) {
+        jobResults.result.forEach((res)=> {
+      const newLi = document.createElement("li");
+      const newSpan = document.createElement("span");
+      newSpan.innerText = `${res.title}`;
+      newLi.appendChild(newSpan);
+      //newLi.innerText += `    Location:`;
+
+      const newSpan2 = document.createElement("span");
+      newSpan2.innerText = ` ${res.location}`;
+      newLi.appendChild(newSpan2);
+      //newLi.innerText = ` ${res.location}`;// innerText preferito a innerHtml per una questione di sicurezza.
+    
+      listLocation.appendChild(newLi);
+    })
+  }else {
+    const newLi = document.createElement("li");
+      newLi.innerText= `No results matching`;
+      listLocation.appendChild(newLi);
+  }
+
 }
+
+buttonListen.addEventListener("click", printResults);
+
+
